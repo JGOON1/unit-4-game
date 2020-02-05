d4065d97b3c06a7735269ef84a6ea9aab31e9a63
john Meng <john.meng014@gmail.com>
2020-02-05 15:27:12 -0500

    changed the formating and layout of page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,19 +16,19 @@ $(document).ready(function () {
         // each gem is assign random value between 1-12 in the beginning. and given an a attribut value.
         var gem1 = Math.floor(Math.random() * 12) + 1;
         $("#gem1").attr("minValue", gem1);
-        // console.log(gem1);
+        console.log(gem1);
         
         var gem2 = Math.floor(Math.random() * 12) + 1;
         $("#gem2").attr("minValue", gem2);
-        // console.log(gem2);
+        console.log(gem2);
         
         var gem3 = Math.floor(Math.random() * 12) + 1;
         $("#gem3").attr("minValue", gem3);
-        // console.log(gem3);
+        console.log(gem3);
         
         var gem4 = Math.floor(Math.random() * 12) + 1;
         $("#gem4").attr("minValue", gem4);
-        // console.log(gem4);
+        console.log(gem4);
         
     }
         
